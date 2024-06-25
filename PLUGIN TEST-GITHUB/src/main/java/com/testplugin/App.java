d926dd17c933d3c29a51571b52060b6ea62ff7c1
SAX-TRECH <qdjdj73@gmail.com>
2024-06-25 16:21:29 -0600

    Add files via upload

---
+++ PLUGIN TEST-GITHUB/src/main/java/com/testplugin/App.java
@@ -0,0 +1,22 @@
+
+package testplugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends JavaPlugin
+{
+
+    @Override
+    public void onEnable(){
+        getLogger().info("Iniciando Plugin");
+    }
+
+    @Override
+    public void onDisable(){
+        getLogger().info("Deshabilitando Plugin");
+    }
+
+}
